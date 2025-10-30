62ed43381bcdaa84af50f43ff5c84c83dd806491
tanvi183 <mneshat7@gmail.com>
2025-10-30 00:16:16 -0700

    data send from backend

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express"); // First to import expresss
+const cors = require('cors'); // without is not provide data to fontend
 const app = express(); // create app
 const port = process.env.PORT || 3000; // If in environment port are not assign than use this port.
 
-app.get("/", (req, res) => {
+app.use(cors()); // call cors
+
+app.get("/users", (req, res) => {
   // req is used for catch request from comming req send response after work
-  res.send("users server is avaiable");
+  res.send(users);
 });
+
+const users = [
+  { id: 1, name: "tanvir", email: "ab@gmail.com" },
+  { id: 2, name: "Sana ullah", email: "abcd@gmail.com" },
+  { id: 3, name: "Hridoy", email: "fgfb@gmail.com" },
+];
 
 app.listen(port, () => {
   // last set app in port to listen
